1a8575f56d0164de3804437b04e73efb32dbdce9
Chen <chen@MacBook-Air-90.local>
2026-06-17 10:33:00 -0400

    small modification

--- graph.cpp
+++ graph.cpp
@@ -3,13 +3,20 @@
 
 class Vertex {
     public: 
-        int val; 
-        std::vector<Vertex*> neighbours; 
+        std::vector<int> vals; 
+        std::vector<Edge*> edges; 
         Vertex* next; 
-        bool wildcard;
-        std::vector<Vertex*> spans;
 
-        Vertex(int val) : val(val), next(nullptr) {}
+        Vertex(std::vector<int> vals) : vals(vals), next(nullptr) {}
+};
+
+class Edge {
+    public: 
+        bool is_span; 
+        Vertex* prev; 
+        Vertex* next;
+
+    Edge(bool is_span, Vertex* prev, Vertex* next) : is_span(is_span), prev(prev), next(next) {}
 };
 
 class Graph {        
@@ -27,8 +34,8 @@ class Graph {
             }
         }
 
-        void add_vertex(int val) {
-            Vertex* v = new Vertex(val); 
+        void add_vertex(std::vector<int> vals) {
+            Vertex* v = new Vertex(vals); 
 
             if (head == nullptr) {
                 head = v; 
@@ -43,65 +50,44 @@ class Graph {
             }
         }
 
-        bool add_edge(int val1, int val2) {
-            Vertex* v1 = find_vertex(val1); 
-            Vertex* v2 = find_vertex(val2); 
+        //TODO: rerewrite add_edge
+        // bool add_edge(int val1, int val2) {
+        //     Vertex* v1 = find_vertex(val1); 
+        //     Vertex* v2 = find_vertex(val2); 
             
-            if (v1 != nullptr && v2 != nullptr) {
-                v1->neighbours.push_back(v2); 
-                v2->neighbours.push_back(v1); 
-                return true; 
-            }
-            return false; 
-        }
+        //     if (v1 != nullptr && v2 != nullptr) {
+        //         v1->neighbours.push_back(v2); 
+        //         v2->neighbours.push_back(v1); 
+        //         return true; 
+        //     }
+        //     return false; 
+        // }
 
-        std::vector<int> get_neighbours(int val) {
-            std::vector<int> neighbour_val; 
-            Vertex* v = find_vertex(val); 
-            if (v!= nullptr) {
-                for (Vertex* neighbour : v->neighbours) {
-                    neighbour_val.push_back(neighbour->val); 
-                }
-            }
-            return neighbour_val; 
-        }
+        //TODO: rewrite get_neighbours
+        // std::vector<int> get_neighbours(int val) {
+        //     std::vector<int> neighbour_val; 
+        //     Vertex* v = find_vertex(val); 
+        //     if (v!= nullptr) {
+        //         for (Vertex* neighbour : v->neighbours) {
+        //             neighbour_val.push_back(neighbour->val); 
+        //         }
+        //     }
+        //     return neighbour_val; 
+        // }
 
     private: 
-        Vertex* find_vertex(int val) {
-            Vertex* current = head; 
-            while (current != nullptr) {
-                if (current->val == val) {
-                    return current; 
-                }
-            }
-            return nullptr; 
-        }
+        //TODO: rewrite find_vertex
+        // Vertex* find_vertex(int val) {
+        //     Vertex* current = head; 
+        //     while (current != nullptr) {
+        //         if (current->val == val) {
+        //             return current; 
+        //         }
+        //     }
+        //     return nullptr; 
+        // }
 };
 
 int main() {
-    Graph g = Graph(); 
-    g.add_vertex(0); 
-    g.add_vertex(1); 
-    g.add_vertex(2);
-    g.add_edge(0, 1); 
-    g.add_edge(0, 2); 
-    g.add_edge(1, 2); 
-
-    std::vector<int> zero_neighbours = g.get_neighbours(0); 
-    std::vector<int> one_neighbours = g.get_neighbours(1); 
-    std::vector<int> two_neighbours = g.get_neighbours(2); 
-    for (int val : zero_neighbours) {
-        std::cout << val << " "; 
-    }
-    std::cout << "\n"; 
-    for (int val : one_neighbours) {
-        std::cout << val << " "; 
-    }
-    std::cout << "\n"; 
-    for (int val : two_neighbours) {
-        std::cout << val << " "; 
-    }
-    std::cout << "\n"; 
-
     return 0; 
 }
